cafd60314cdbdc473c689d68892e6f6f938da5b1
swelanauguste <swelanauguste@gmail.com>
2024-11-27 13:43:29 -0400

    createsuperuser

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -15,7 +15,7 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
 # users
