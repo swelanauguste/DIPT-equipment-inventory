40b6ab53f2135924a971a1e4cfc35e9295d9ba3a
swelanauguste <swelanauguste@gmail.com>
2024-11-27 13:17:35 -0400

    deploy to server

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -18,9 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zc)6!7(rap5n*@!#3h#)bdtt&ix@@sq#wl9friuo&tp-%9qv+^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+DEBUG = bool(int(os.environ.get("DEBUG", default=0)))
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 
 # Application definition
@@ -77,24 +80,24 @@ WSGI_APPLICATION = "cf.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# if DEBUG:
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#             "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#             "USER": os.environ.get("SQL_USER", "user"),
-#             "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#             "HOST": os.environ.get("SQL_HOST", "localhost"),
-#             "PORT": os.environ.get("SQL_PORT", "5432"),
-#         }
-#     }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+            "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+            "USER": os.environ.get("SQL_USER", "user"),
+            "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+            "HOST": os.environ.get("SQL_HOST", "localhost"),
+            "PORT": os.environ.get("SQL_PORT", "5432"),
+        }
+    }
 
 
 # Password validation
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,35 +1,49 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py makemigrations
+python manage.py migrate
+# python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
-# # users
-# python manage.py loaddata ./static/docs/users/department_list.json
-# python manage.py loaddata ./static/docs/users/location_list.json
-# python manage.py add_user_list
+# users
+python manage.py loaddata ./static/docs/users/department_list.json
+python manage.py loaddata ./static/docs/users/location_list.json
+python manage.py add_user_list
+python manage.py update_technician_role
 
-# # computers
-# python manage.py loaddata ./static/docs/computers/status_list.json
-# python manage.py loaddata ./static/docs/computers/operating_system_list.json
-# python manage.py loaddata ./static/docs/computers/maker_list.json
-# python manage.py loaddata ./static/docs/computers/monitor_model_list.json
-# python manage.py loaddata ./static/docs/computers/monitor_list.json
-# python manage.py loaddata ./static/docs/computers/computer_model_list.json
-# python manage.py add_computer_list
-# python manage.py loaddata ./static/docs/computers/microsoft_version_list.json
-# python manage.py loaddata ./static/docs/computers/microsoft_list.json
+# computers
+python manage.py loaddata ./static/docs/computers/status_list.json
+python manage.py loaddata ./static/docs/computers/operating_system_list.json
+python manage.py loaddata ./static/docs/computers/maker_list.json
+python manage.py loaddata ./static/docs/computers/monitor_model_list.json
+python manage.py loaddata ./static/docs/computers/monitor_list.json
+python manage.py loaddata ./static/docs/computers/computer_model_list.json
+python manage.py add_computer_list
+python manage.py loaddata ./static/docs/computers/microsoft_version_list.json
+python manage.py loaddata ./static/docs/computers/microsoft_list.json
 
-# # printers
-# python manage.py loaddata ./static/docs/printers/printer_model_list.json
-# python manage.py loaddata ./static/docs/printers/printer_list.json
+# printers
+python manage.py loaddata ./static/docs/printers/printer_model_list.json
+python manage.py loaddata ./static/docs/printers/printer_list.json
 
-# # tickets
-# python manage.py loaddata ./static/docs/tickets/ticket_status_list.json
-# python manage.py loaddata ./static/docs/tickets/ticket_category_list.json
-# python manage.py add_ticket_list
-# python manage.py loaddata ./static/docs/tickets/ticket_comment_list.json
-# python manage.py add_ticket_comment_list
+# tickets
+python manage.py loaddata ./static/docs/tickets/ticket_status_list.json
+python manage.py loaddata ./static/docs/tickets/ticket_category_list.json
+python manage.py add_ticket_list
+python manage.py loaddata ./static/docs/tickets/ticket_comment_list.json
+python manage.py add_ticket_comment_list
 
 exec "$@"
