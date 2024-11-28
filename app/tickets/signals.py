c6159a6c34149d841a05a4751af523f03d9f373e
swelanauguste <swelanauguste@gmail.com>
2024-11-28 10:53:08 -0400

    entrypoint

--- app/tickets/signals.py
+++ app/tickets/signals.py
@@ -11,29 +11,29 @@
 # )
 
 
-# @receiver(post_save, sender=Comment)
-# def ticket_comment_created_email_signal(sender, instance, created, **kwargs):
-#     if created:
-#         ticket_comment_created_email_thread = threading.Thread(
-#             target=ticket_comment_created_email, args=(instance,)
-#         )
-#         ticket_comment_created_email_thread.start()
-#     # else:
-#     #     ticket_update_email_thread = threading.Thread(
-#     #         target=ticket_updated_email, args=(instance,)
-#     #     )
-#     #     ticket_update_email_thread.start()
+@receiver(post_save, sender=Comment)
+def ticket_comment_created_email_signal(sender, instance, created, **kwargs):
+    if created:
+        ticket_comment_created_email_thread = threading.Thread(
+            target=ticket_comment_created_email, args=(instance,)
+        )
+        ticket_comment_created_email_thread.start()
+    # else:
+    #     ticket_update_email_thread = threading.Thread(
+    #         target=ticket_updated_email, args=(instance,)
+    #     )
+    #     ticket_update_email_thread.start()
 
 
-# @receiver(post_save, sender=Ticket)
-# def ticket_created_email_signal(sender, instance, created, **kwargs):
-#     if created:
-#         ticket_created_email_thread = threading.Thread(
-#             target=ticket_created_email, args=(instance,)
-#         )
-#         ticket_created_email_thread.start()
-#     else:
-#         ticket_update_email_thread = threading.Thread(
-#             target=ticket_updated_email, args=(instance,)
-#         )
-#         ticket_update_email_thread.start()
+@receiver(post_save, sender=Ticket)
+def ticket_created_email_signal(sender, instance, created, **kwargs):
+    if created:
+        ticket_created_email_thread = threading.Thread(
+            target=ticket_created_email, args=(instance,)
+        )
+        ticket_created_email_thread.start()
+    else:
+        ticket_update_email_thread = threading.Thread(
+            target=ticket_updated_email, args=(instance,)
+        )
+        ticket_update_email_thread.start()
