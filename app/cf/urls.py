63a4e9c0d61ccb8cf320da7bb676c2132fc31353
swelanauguste <swelanauguste@gmail.com>
2024-12-02 07:36:40 -0400

    adding to cf urls debug urls

--- app/cf/urls.py
+++ app/cf/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,3 +11,7 @@ urlpatterns = [
     path("printers/", include("printers.urls")),
     path("users/", include("users.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
