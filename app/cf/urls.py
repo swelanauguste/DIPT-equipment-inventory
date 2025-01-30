9e6badec8eb537ef870f8ac87c6654a7a19ed55e
swelanauguste <swelanauguste@gmail.com>
2025-01-30 09:57:10 -0400

    update admin URL path to 'dipt/' for improved routing clarity

--- app/cf/urls.py
+++ app/cf/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("dipt/", admin.site.urls),
     path("tickets/", include("tickets.urls")),
     path("computers/", include("computers.urls")),
     path("office-keys/", include("officekeys.urls")),
